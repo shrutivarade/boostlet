6afc028face87bbc73eea49d223ff489eba4beb9
Shruti Varade <shrutivarade27@gmail.com>
2024-05-13 11:18:15 -0400

    feat (Plotly) - requestAnimationFrames()

--- examples/plotly.js
+++ examples/plotly.js
@@ -1,17 +1,18 @@
 script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://boostlet.org/dist/boostlet.min.js";
+// script.src = "https://boostlet.org/dist/boostlet.min.js";
+script.src = "http://localhost:8000/dist/boostlet.min.js";
 
 script.onload = run;
 document.head.appendChild(script);
 eval(script);
 
+
+
 function run() {
-  
+
   // detect visualization framework
   Boostlet.init();
-
-  all_pixels = Boostlet.get_image().data;
 
   // load Plotly.js library and start plotting in the onload callback
   Boostlet.load_script('https://cdn.plot.ly/plotly-2.25.2.min.js', plot);
@@ -20,23 +21,37 @@ function run() {
 
 function plot() {
 
-  // create div for plot
-  let container = window.document.createElement('div');
-  container.id = 'plotlyDiv';
-  container.style.position = 'absolute';
-  container.style.top = '10px';
-  container.style.left = '10px';
-  container.style.zIndex = '1000';
-  container.onclick = function() {
-    // destroy on click
-    window.document.body.removeChild(container);
+  // create div for plot if it doesn't exist
+  let container = document.getElementById('plotlyDiv');
+  if (!container) {
+    container = window.document.createElement('div');
+    container.id = 'plotlyDiv';
+    container.style.position = 'absolute';
+    container.style.top = '10px';
+    container.style.left = '10px';
+    container.style.zIndex = '1000';
+    container.onclick = function() {
+      // destroy on click
+      window.document.body.removeChild(container);
+    }
+    window.document.body.appendChild(container);
   }
-  window.document.body.appendChild(container);
 
-  // plot the data
-  Plotly.newPlot('plotlyDiv', [{
+  updatePlot();
+
+}
+
+function updatePlot() {
+  
+  all_pixels = Boostlet.get_image().data;
+
+  // update the data
+  Plotly.react('plotlyDiv', [{
     x: all_pixels,
     type: 'histogram'
   }]);
 
+  console.log("updated");
+  // request next frame
+  requestAnimationFrame(updatePlot);
 }
